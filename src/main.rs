fc01a3babffdc5134b7c41dc069f3b37b7bb6084
Luca Meni <76182091+lucameni@users.noreply.github.com>
2021-01-30 23:10:49 -0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,13 @@
-// Fibonacci Number Generator
-
 fn main() {
     let st_num = [0,1];
-    let mut p_num = st_num[0];
+    let mut prev_num = st_num[0];
     let mut a_num = st_num[1];
 
     for rep in 1..12 {
-        let temp = p_num + a_num;
-        p_num = a_num;
-        a_num = temp;
+        let temp = prev_num + next_num;
+        prev_num = next_num;
+        next_num = temp;
 
-        println!("Your number in rep {} is: {}", rep, temp);
+        println!("Your number in repetition {} is: {}", rep, temp);
     }
 }
